97aae1938f3a509836e1abc6078f2ac2af4b747e
Toni-Ty <antonique2479@yahoo.com>
2019-02-03 01:46:46 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -33,6 +33,22 @@ array.sort { |a, b|   b <=> a} #this method block eliminates do and end, and is 
 end
 
 def sort_array_char_count (array)
-  array.sort { |a|   a.length}
+  array.sort { |a, b|  a.length <=> b.length}
 end
-  
+
+array = ["a", "b", "c", "d", "e", "f", "g"]
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+  def reverse_array(array)
+    array.reverse
+  end
+
+def kesha_maker(array)
+  array.each do |item|
+    item[2] = "$"
+    array
+  end
+end
